b06b5d58a25835a20ebcc5b652f79b15af50ecdc
Vlad <vladgorbich@gmail.com>
2018-07-15 12:37:34 -0500

    Stup test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 // Setup mongoose to use promises
 mongoose.Promise = global.Promise;
 // Plug Heroku (mLab) addon or local database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
